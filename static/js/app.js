9c029b55aaca1d8a37edab92702060962154335b
Simon <ogbanjesi@gmail.com>
2025-04-03 11:54:17 +0100

    add functional menubar using JavaScript

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,21 @@
 let menu_bar = document.getElementById('navbar-btn')
-let navnar_items = document.getElementsByClassName('navbar-items')
+let navbar_items = document.getElementsByClassName('navbar-items')
+
+
+menu_bar.addEventListener('click', ()=>{
+    //console.log('Manu clicked')
+    
+    for(let i=0; i<navbar_items.length; i++){
+    navbar_items[i].classList.toggle('d-md-none,')
+  
+    }
+})
+
+
+/*let fruit = ["mango", "orange", "banana"]
+
+console.log(fruit[0])
+console.log(fruit[1])
+console.log(fruit[2])
+
+console.log(fruit.length)*/
