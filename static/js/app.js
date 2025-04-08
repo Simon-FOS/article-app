b4e18e4a738a944f2b72b4065792a561866e01c5
Simon <ogbanjesi@gmail.com>
2025-04-08 12:29:28 +0100

    understanding the concept of JSON and javascrip DOM

--- static/js/app.js
+++ static/js/app.js
@@ -12,6 +12,131 @@ menu_bar.addEventListener('click', ()=>{
 })
 
 
+
+let user_info = [
+    {
+        id: 1,
+        first_name: "Simon",
+        last_name: "Ogbanje",
+        is_admin: true,
+        age : 20,
+        is_authenticated: true
+    },
+
+    {
+        id: 2,
+        first_name: "Daniel",
+        last_name: "Ogbanje",
+        is_admin: false,
+        age : 20,
+        is_authenticated: true
+    },
+
+    {
+        id: 3,
+        first_name: "Esther",
+        last_name: "Fehintola",
+        is_admin: false,
+        age : 20,
+        is_authenticated: false
+    }
+]
+
+let article_data = [
+    {
+        id:1,
+        title: "This is title 1",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/mmm.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    },
+
+    {
+        id:2,
+        title: "This is title 2",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/m2.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    },
+
+    {
+        id:3,
+        title: "This is title 3",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/m3.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    },
+
+    {
+        id:3,
+        title: "This is title 3",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/m3.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    },
+
+    {
+        id:3,
+        title: "This is title 3",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/m3.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    },
+
+    {
+        id:3,
+        title: "This is title 3",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repudiandae est quidem, nihil, dicta modi porro minus maiores pariatur ipsa laudantium perferendis eum, expedita doloremque facilis ducimus odio. Dolores, perspiciatis.",
+        Image_url: "static/img/m3.jpg",
+        url: "/article/1",
+        date: new Date().toDateString()
+    }
+]
+
+window.localStorage.setItem('data', article_data)
+
+let data = window.localStorage.getItem('data')
+
+console.log(data)
+
+
+let articleEl = document.getElementById('article-list')
+
+for (let i=0; i<article_data.length; i++){
+    let data = article_data[i]
+    articleEl.insertAdjacentHTML("afterbegin", `
+      <div class="col-md-4">
+        <div class="card">
+            <div class="card-img">
+                <img src=${data.Image_url} alt="">
+            </div>
+
+            <div class="card-content">
+                <a href=${data.url} class="btn card-btn">
+                    View Article
+                </a>
+
+                <div>
+                    <h2 class="card-title">
+                        ${data.title}
+                    </h2>
+                    
+                    <div class="card-date">
+                        ${data.date}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>  
+    `)
+}
+
 /*let fruit = ["mango", "orange", "banana"]
 
 console.log(fruit[0])
