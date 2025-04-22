c6716f9116271e2ff74ed1acbf94404cca271229
Simon <ogbanjesi@gmail.com>
2025-04-22 13:03:45 +0100

    handle form using javascript

--- static/js/app.js
+++ static/js/app.js
@@ -2,28 +2,28 @@ let menu_bar = document.getElementById('navbar-btn')
 let navbar_items = document.getElementsByClassName('navbar-items')
 
 
-menu_bar.addEventListener('click', ()=>{
+menu_bar.addEventListener('click', () => {
     //console.log('Manu clicked')
-    
-    for(let i=0; i<navbar_items.length; i++){
-    navbar_items[i].classList.toggle('d-md-none')
-  
+
+    for (let i = 0; i < navbar_items.length; i++) {
+        navbar_items[i].classList.toggle('d-md-none')
+
     }
 })
 
 //FETCH ARTICLE DATA
 fetch("/json/article.json")
-    .then((response)=>{
+    .then((response) => {
         return response.json()
     })
-    .then((data)=>{
-        
+    .then((data) => {
+
         let articleEl = document.getElementById('article-list')
 
-        for(let i=0; i<9; i++){
+        for (let i = 0; i < 9; i++) {
             let article = data[i];
-            
-            articleEl.insertAdjacentHTML('afterbegin',`
+
+            articleEl.insertAdjacentHTML('afterbegin', `
                 <div class="col-md-4">
                     <div class="card">
                         <div class="card-img">
@@ -52,9 +52,24 @@ fetch("/json/article.json")
         }
     })
 
-    .catch((error)=>{
+    .catch((error) => {
         console.log(error)
     })
+
+//CREATE NEW ARTICLE
+let form_data = document.getElementById('creat-article-form');
+
+form_data.addEventListener('submit', (event) => {
+    event.preventDefault(); //prevent default form submission
+
+    console.log('creating new article');
+
+    let converted_form = new FormData(form_data) //converting html from to javascript
+
+    converted_form = Object.fromEntries(converted_form) //converts javescrip form to js objects
+
+    console.log(converted_form)
+})
 
 
 /*
